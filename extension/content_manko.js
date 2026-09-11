cc4b1ab991e3ca08d8a1f0effec80313cb960b24
hmtnvac-cpu <hmtnvac@gmail.com>
2026-09-11 18:43:44 +0700

    Add Manko page detector

---
+++ extension/content_manko.js
@@ -0,0 +1,31 @@
+(() => {
+  function findPlayerUrl() {
+    const iframes = [...document.querySelectorAll('iframe')];
+    for (const el of iframes) {
+      const src = el.src || el.getAttribute('src') || '';
+      if (src.includes('https://javplayer.cc/e/')) return src;
+    }
+    const html = document.documentElement.innerHTML.replace(/\\\//g, '/');
+    const m = html.match(/https:\/\/javplayer\.cc\/e\/[^"'<>\\s]+/);
+    return m ? m[0].replace(/&amp;/g, '&') : null;
+  }
+
+  function send() {
+    const playerUrl = findPlayerUrl();
+    if (!playerUrl) return false;
+    chrome.runtime.sendMessage({
+      type: 'MANKO_PLAYER_FOUND',
+      movieUrl: location.href,
+      title: document.title,
+      playerUrl
+    });
+    return true;
+  }
+
+  if (send()) return;
+  const obs = new MutationObserver(() => {
+    if (send()) obs.disconnect();
+  });
+  obs.observe(document.documentElement, {subtree:true, childList:true, attributes:true, attributeFilter:['src']});
+  setTimeout(() => obs.disconnect(), 30000);
+})();
